a35b045284d0ce15118f20208b571d1c32b54caf
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2013-12-31 12:07:46 +0400

    Added script to transpose tabs

---
+++ examples/transpose.py
@@ -0,0 +1,79 @@
+import os
+
+import guitarpro
+
+def unfold_tracknumber(tracknumber, tracks):
+    '''Substitute '*' with all track numbers except for percussion tracks.'''
+    if tracknumber == '*':
+        for number, track in enumerate(tracks, start=1):
+            if not track.isPercussionTrack:
+                yield number
+    else:
+        yield tracknumber
+
+def transpose(track, semitone, stringmap):
+    for measure in track.measures:
+        for beat in measure.beats:
+            for voice in beat.voices:
+                for note in voice.notes:
+                    if (1 << (note.string - 1) & stringmap and 
+                            not note.effect.deadNote):
+                        note.value += semitone
+                        if note.value < 0:
+                            print ("Warning on track %d '%s', measure %d, beat %d" %
+                                   (track.number, track.name, measure.number(), beat.number))
+                            import pdb; pdb.set_trace()
+
+def main(source, dest, tracks, semitones, stringmaps):
+    if tracks is None:
+        tracks = ['*']
+    if stringmaps is None:
+        stringmaps = [0x7f] * len(tracks)
+    song = guitarpro.parse(source)
+    for number, semitone, stringmap in zip(tracks, semitones, stringmaps):
+        for number in unfold_tracknumber(number, song.tracks):
+            track = song.tracks[number - 1]
+            transpose(track, semitone, stringmap)
+    if dest is None:
+        dest = '%s-transposed%s' % os.path.splitext(source)
+    guitarpro.write(song, dest)
+
+if __name__ == '__main__':
+    import argparse
+
+    def bitarray(string):
+        return int(string, base=2)
+
+    def trackumber(string):
+        if string == '*':
+            return string
+        else:
+            return int(string)
+
+    description = """
+        Transpose tracks of GP tab by N semitones. 
+        Multiple '--track' and '--by' arguments can be specified.
+        """
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('source',
+                        metavar='SOURCE',
+                        help='path to the source tab')
+    parser.add_argument('dest',
+                        metavar='DEST', nargs='?',
+                        help='path to the processed tab')
+    parser.add_argument('-t', '--track',
+                        metavar='NUMBER', type=trackumber,
+                        dest='tracks', action='append',
+                        help='number of the track to transpose')
+    parser.add_argument('-b', '--by',
+                        metavar='N', type=int, required=True,
+                        dest='semitones', action='append',
+                        help='transpose by N steps')
+    parser.add_argument('-s', '--stringmap',
+                        metavar='BITARRAY', type=bitarray,
+                        dest='stringmaps', action='append',
+                        help='bit array where ones represent strings that should '
+                             'be transposed')
+    args = parser.parse_args()
+    kwargs = dict(args._get_kwargs())
+    main(**kwargs)
