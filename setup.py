f8b20aee20848f57420043286caea16a45c51ff8
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-11-21 16:13:33 +0300

    Move metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,54 +1,9 @@
 import setuptools
 
-setup_requires = ['pytest-runner']
-
-install_requires = [
-    'attrs>=19.2',
-]
-
-tests_require = [
-    'pytest',
-]
-
-with open('README.rst') as fp:
-    README = fp.read()
-
 setuptools.setup(
     name='PyGuitarPro',
-    description='Read, write, and manipulate GP3, GP4 and GP5 files.',
-    long_description=README,
     version='0.8.dev',
-    author='Sviatoslav Abakumov',
-    author_email='dust.harvesting@gmail.com',
-    url='https://github.com/Perlence/PyGuitarPro',
-    platforms=['Windows', 'POSIX', 'Unix', 'MacOS X'],
-    license='LGPLv3',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    setup_requires=setup_requires,
-    install_requires=install_requires,
-    tests_require=tests_require,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Natural Language :: English',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Artistic Software',
-        'Topic :: Multimedia :: Sound/Audio',
+    install_requires=[
+        'attrs>=19.2',
     ],
 )
